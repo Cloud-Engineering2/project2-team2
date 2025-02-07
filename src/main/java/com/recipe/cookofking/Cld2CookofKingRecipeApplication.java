03153c33852275db02d7601921e423accbc48d59
KangSeongKwan <kang53497@naver.com>
2025-02-07 12:57:35 +0900

    First Commit - KSK

---
+++ src/main/java/com/recipe/cookofking/Cld2CookofKingRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.recipe.cookofking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cld2CookofKingRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cld2CookofKingRecipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recipe/cookofking/Cld2CookofKingRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipe.cookofking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cld2CookofKingRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
