53176c21de10802205d23bab2e84856f7fe63c07
KangSeongKwan <kang53497@naver.com>
2025-02-10 13:33:03 +0900

    Make View Like but Returned 500 Error Version

--- src/main/java/com/recipe/cookofking/service/LikeService.java
+++ src/main/java/com/recipe/cookofking/service/LikeService.java
@@ -1,6 +1,27 @@
 package com.recipe.cookofking.service;
+
+import com.recipe.cookofking.dto.LikeDto;
+import com.recipe.cookofking.repository.LikeRepository;
+import com.recipe.cookofking.repository.PostRepository;
 import org.springframework.stereotype.Service;
+
 @Service
 public class LikeService {
     // Add Like related business logic here
+    private final LikeRepository likeRepository;
+    private final PostRepository postRepository;
+
+    public LikeService(LikeRepository likeRepository, PostRepository postRepository) {
+        this.likeRepository = likeRepository;
+        this.postRepository = postRepository;
+    }
+
+    public LikeDto getLikes(Integer postId) {
+        if (!postRepository.existsById(postId)) {
+            throw new IllegalArgumentException("Post not found");
+        }
+
+        long likesCount = likeRepository.countByPostId(postId);
+        return new LikeDto();
+    }
 }
