30e242494207a0fbd3adb51df114a83854d7c670
amberlee0201 <amber.lee.020100@gmail.com>
2025-02-11 16:07:46 +0900

    :sparkles: feature: delete stale images (incomplete)

---
+++ src/main/java/com/recipe/cookofking/config/SchedulerConfig.java
@@ -0,0 +1,9 @@
+package com.recipe.cookofking.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling  // 스케줄링 활성화
+public class SchedulerConfig {
+}
