62dbd2ce6a90066e0788835df0f586472aa5ffd4
phc979 <parkhancheol97@gmail.com>
2025-02-11 14:53:24 +0900

    feature: post-remove & refactoring for session

--- src/main/java/com/recipe/cookofking/repository/ImagemappingRepository.java
+++ src/main/java/com/recipe/cookofking/repository/ImagemappingRepository.java
@@ -1,6 +1,5 @@
 package com.recipe.cookofking.repository;
 import com.recipe.cookofking.entity.Imagemapping;
-import com.recipe.cookofking.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
